a98a3ff4e3ceaf220a2acc04514ee5663c7cbc83
StephenKautt <100315266+StephenKautt@users.noreply.github.com>
2025-03-16 23:12:50 -0500

    working VLM

--- utils/prompt_processing.py
+++ utils/prompt_processing.py
@@ -2,5 +2,6 @@
 def create_prompt(detections, user_prompt):
     if not detections:
         return f"The image contains no detected objects. {user_prompt}"
+
     object_list = ", ".join([d["class"] for d in detections])
-    return user_prompt
+    return f"The image contains the following objects: {object_list}. {user_prompt}"
